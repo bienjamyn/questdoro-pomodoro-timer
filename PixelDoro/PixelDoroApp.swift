6af686bb28fbda65a2c929fe78dee3340830c8c6
Kevin Tayong <kevintayong@gmail.com>
2025-12-19 14:37:56 -0500

    Initial Commit

---
+++ PixelDoro/PixelDoroApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixelDoroApp.swift
+//  PixelDoro
+//
+//  Created by Kevin Tayong on 2025-12-19.
+//
+
+import SwiftUI
+
+@main
+struct PixelDoroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
